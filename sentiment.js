a8b77bfb1ea9a9480452e7e869a2fb543ec2827f
Graeme Wolfendale <gwolfendale@gmail.com>
2014-10-16 04:38:36 +0100

    Create sentiment.js

---
+++ sentiment.js
@@ -0,0 +1,34 @@
+//sentimental 
+//copyright Graeme Wolfendale 2014 GPL version 2 license
+
+//sentiment analysis tool
+
+
+//take text as input
+  var input = document.getElementById("intext").value;
+  input = input.toString();
+
+//extract the objects of sentiment
+  //split the input into words in an array
+  var inwords = 
+  //for each word 
+  for(i=0,i<=inwords.length()){
+    //classify the word as an object of sentiment, or not
+    if(inwords[i]==*NOUN*){
+    }
+    //classify the word as a sentiment, or not
+    if(inwords[i]==*SENTIMENT*){
+      //classify the sentiment as positive or negative
+      //identify the object this sentiment is directed towards
+      //for positive sentiment, increment the object's positive sentiment count
+      //for negative sentiment, increment the object's negative sentiment count
+      //inc/decrement the object's overall sentiment for positive or negative sentiment, respectively
+    }
+  }
+
+//display output 
+
+//for each object
+  //output positive sentiment count
+  //output negative sentiment count
+  //output overall sentiment
