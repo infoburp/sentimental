ea18fcde779826fca64a7335dd3c2e8aaee62d25
Graeme Wolfendale <gwolfendale@gmail.com>
2014-10-16 04:43:58 +0100

    Update sentiment.js

--- sentiment.js
+++ sentiment.js
@@ -3,32 +3,40 @@
 
 //sentiment analysis tool
 
+function analyse(intext){
+  return sentiment(intext);
+}
 
-//take text as input
-  var input = document.getElementById("intext").value;
-  input = input.toString();
-
-//extract the objects of sentiment
-  //split the input into words in an array
-  var inwords = 
-  //for each word 
-  for(i=0,i<=inwords.length()){
-    //classify the word as an object of sentiment, or not
-    if(inwords[i]==*NOUN*){
+function sentiment(intext){
+  //take text as input
+    var input = document.getElementById("intext").value;
+    input = input.toString();
+  
+  //extract the objects of sentiment
+    //split the input into words in an array
+    var inwords = 
+    //for each word 
+    for(i=0,i<=inwords.length()){
+      //classify the word as an object of sentiment, or not
+      if(inwords[i]==*NOUN*){
+      }
+      //classify the word as a sentiment, or not
+      if(inwords[i]==*SENTIMENT*){
+        //classify the sentiment as positive or negative
+        //identify the object this sentiment is directed towards
+        //for positive sentiment, increment the object's positive sentiment count
+        //for negative sentiment, increment the object's negative sentiment count
+        //inc/decrement the object's overall sentiment for positive or negative sentiment, respectively
+      }
     }
-    //classify the word as a sentiment, or not
-    if(inwords[i]==*SENTIMENT*){
-      //classify the sentiment as positive or negative
-      //identify the object this sentiment is directed towards
-      //for positive sentiment, increment the object's positive sentiment count
-      //for negative sentiment, increment the object's negative sentiment count
-      //inc/decrement the object's overall sentiment for positive or negative sentiment, respectively
-    }
-  }
+  
+  //prepare output array 
+  
+  //for each object
+    //output positive sentiment count
+    //output negative sentiment count
+    //output overall sentiment
 
-//display output 
-
-//for each object
-  //output positive sentiment count
-  //output negative sentiment count
-  //output overall sentiment
+  //return output array
+  return outputarray;
+}
